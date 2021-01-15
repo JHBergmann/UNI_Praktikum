36ee9f0abb24db432ef91c0de1a516cca5ca275a
jhbergmann <me@jhb.onl>
2021-01-15 10:11:23 +0100

    Initial commit

---
+++ src/com/unibw/HYPO.java
@@ -0,0 +1,4 @@
+package com.unibw;
+
+public class HYPO {
+}
---
+++ src/com/unibw/Main.java
@@ -0,0 +1,40 @@
+package com.unibw;
+
+import java.awt.*;
+import java.util.Scanner;
+import com.unibw.SimpleGraphicPanel;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        /**** Window
+        JFrame window = new JFrame();
+        window.setSize(640,480);
+        window.setTitle("UNIBW Praktikum Aufgabe 1");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+        ****/
+
+        /* Hypothenusen Rechner */ /*
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kathete 1:");
+        double k1 = sc.nextDouble();
+        System.out.println("Kathete 2:");
+        double k2 = sc.nextDouble();
+        System.out.println("Hypothenuse");
+        System.out.println(HYPOTH(k1,k2));
+        */
+        SimpleGraphicPanel.drawCircle(SimpleGraphicPanel.getRandomColor(),true, 100,10, 100);
+        SimpleGraphicPanel.drawTriangle(Color.GREEN,true,100,0,200,0,150,100);
+
+    }
+
+    public static double HYPOTH(double k1, double k2) {
+        double HYPO = Math.hypot(k1,k2);
+        return HYPO;
+    }
+
+
+
+}
