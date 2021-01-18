ebde43fc37f58b907ab4d104ed47c78be11a4452
jhbergmann <me@jhb.onl>
2021-01-18 15:16:11 +0100

    Blumen Formeln für H, G

--- src/Praktikum1/Aufgabe2/mainAufgabe2.java
+++ src/Praktikum1/Aufgabe2/mainAufgabe2.java
@@ -25,4 +25,34 @@ public class mainAufgabe2 {
     public static double my(double ax, double ay, double bx, double by) {
         return ay + 0.5 * (by - ay) + 0.5 * (bx - ax);
     }
+
+    public static double t (double ex, double ey, double fx, double fy){
+        return Math.hypot(fy - ey, fx - ex);           // function definition of t
+    }
+    public static double s (double ex, double ey, double fx, double fy){
+        return t(ex, ey, fx, fy) / Math.sqrt(3.0);    // function application of t
+    }
+
+    // Klassenmethoden schreiben
+                // Rückgabewert          // Eingabewerte
+    public static double gx (double ex, double ey, double fx, double fy) {
+        return ex + 0.5 * (fx - ex)
+                + (s(ex, ey, fx, fy) / (2.0 * t(ex, ey, fx, fy))) * (fy - ey);
+    }
+
+    public static double gy (double ex, double ey, double fx, double fy) {
+        return ey + 0.5 * (fy - ey)
+                + (s(ex, ey, fx, fy) / (2.0 * t(ex, ey, fx, fy))) * (ex - fx);
+    }
+
+    public static double hx (double ex, double ey, double fx, double fy) {
+        return ex + 0.5 * (fx - ex)
+                + (s(ex, ey, fx, fy) / (2.0 * t(ex, ey, fx, fy))) * (fy - ey);
+    }
+
+    public static double hy (double ex, double ey, double fx, double fy) {
+        return ey + 0.5 * (fy - ey)
+                + (s(ex, ey, fx, fy) / (2.0 * t(ex, ey, fx, fy))) * (ex - fy);
+    }
+
 }
