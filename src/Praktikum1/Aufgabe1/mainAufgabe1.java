b3583f94c184f5f2a279dbd7b45acdec67ccdf3a
jhbergmann <me@jhb.onl>
2021-01-18 14:54:00 +0100

    Blumen Formeln

--- src/Praktikum1/Aufgabe1/mainAufgabe1.java
+++ src/Praktikum1/Aufgabe1/mainAufgabe1.java
@@ -1,4 +1,16 @@
 package Praktikum1.Aufgabe1;
 
 public class mainAufgabe1 {
+    public static void main(String[] args) {
+        float k1 = 100.0F;
+        float k2 = 75.0F;
+
+        float c = (k1 * k1) + (k2 * k2);
+        float d = (float) Math.sqrt(c);
+
+        float h = (float) Math.hypot(k1, k2);
+
+        System.out.println(h);
+        System.out.println(d);
+    }
 }
--- src/Praktikum1/Aufgabe2/mainAufgabe2.java
+++ src/Praktikum1/Aufgabe2/mainAufgabe2.java
@@ -1,4 +1,28 @@
 package Praktikum1.Aufgabe2;
 
 public class mainAufgabe2 {
+
+    public static double cx(double ax, double ay, double bx, double by) {
+        return bx + ay - by;
+    }
+
+    public static double cy(double ax, double ay, double bx, double by) {
+        return by + bx - ax;
+    }
+
+    public static double dx(double ax, double ay, double bx, double by) {
+        return ax + ay - by;
+    }
+
+    public static double dy(double ax, double ay, double bx, double by) {
+        return ay + bx - ax;
+    }
+
+    public static double mx(double ax, double ay, double bx, double by) {
+        return ax + (bx - ax) / 2.0 + (ay - by) / 2.0;
+    }
+
+    public static double my(double ax, double ay, double bx, double by) {
+        return ay + 0.5 * (by - ay) + 0.5 * (bx - ax);
+    }
 }
